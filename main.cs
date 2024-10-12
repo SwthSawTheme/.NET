8f9cf1de7b7a10380755ed4d3c443ed53d79a05f
SwthSawTheme <sawtheme1@gmail.com>
2024-10-11 22:08:48 -0300

    Parametro Length

--- main.cs
+++ main.cs
@@ -20,4 +20,13 @@ Console.WriteLine($"Hello {Friend}");
 
 // ---------- // ------------
 
-//
+// Trabalhar com cadeias de caracteres
+
+string firstFriend = "Minnv";
+string secondFriend = "Octers";
+Console.WriteLine($"Meu amigo {firstFriend} e {secondFriend}");
+
+// Parametro Length
+
+Console.WriteLine($"O nome {firstFriend} contém {firstFriend.Length} letras");
+Console.WriteLine($"O nome {secondFriend} contém {secondFriend.Length} letras");
--- obj/Debug/net8.0/.NET.AssemblyInfo.cs
+++ obj/Debug/net8.0/.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de4a3c2a179abf946bf7fa5180ad8790033a5da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353559a1c1638af4300f834b01a3d95bfa57d4d3")]
 [assembly: System.Reflection.AssemblyProductAttribute(".NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
