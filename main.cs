5c43be9f044b5205d93dbabb173c835c4588ed5b
SwthSawTheme <sawtheme1@gmail.com>
2024-10-14 20:48:30 -0300

    att

--- main.cs
+++ main.cs
@@ -92,3 +92,6 @@
 int max = int.MaxValue;
 int min = int.MinValue;
 Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 10;
+Console.WriteLine($"An example of overflow: {what}");
--- obj/Debug/net8.0/.NET.AssemblyInfo.cs
+++ obj/Debug/net8.0/.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06dfb784e0b4ed6782740cdcd7756b8aec407904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18472ce70611706e7e7035516a21315e2f969e8")]
 [assembly: System.Reflection.AssemblyProductAttribute(".NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
