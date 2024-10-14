6a3f0d3f4cd97be57bb14502efcb271723585d18
SwthSawTheme <sawtheme1@gmail.com>
2024-10-14 20:56:40 -0300

    calculando a area com um raio 2.50 e usando Math.PI

--- main.cs
+++ main.cs
@@ -109,10 +109,14 @@
 // double third = 1.0 / 3.0;
 // Console.WriteLine(third);
 
-double a = 1.0;
-double b = 3.0;
-Console.WriteLine(a / b);
+// double a = 1.0;
+// double b = 3.0;
+// Console.WriteLine(a / b);
 
-decimal c = 1.0M;
-decimal d = 3.0M;
-Console.WriteLine(c / d);
+// decimal c = 1.0M;
+// decimal d = 3.0M;
+// Console.WriteLine(c / d);
+
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
