a5f2ec4d36d376d690cd6e129d33d3f3d8991e18
SwthSawTheme <sawtheme1@gmail.com>
2024-10-18 22:55:43 -0300

    att

--- main.cs
+++ main.cs
@@ -139,16 +139,16 @@
 // else
 //     Console.WriteLine("O valor é menor que 10");
 
-int a = 5;
-int b = 3;
-int c = 4;
-if ((a + b + c > 10) && (a == b))
-{
-    Console.WriteLine("The answer is greater than 10");
-    Console.WriteLine("And the first number is equal to the second");
-}
-else
-{
-    Console.WriteLine("The answer is not greater than 10");
-    Console.WriteLine("Or the first number is not equal to the second");
-}
+// int a = 5;
+// int b = 3;
+// int c = 4;
+// if ((a + b + c > 10) && (a == b))
+// {
+//     Console.WriteLine("The answer is greater than 10");
+//     Console.WriteLine("And the first number is equal to the second");
+// }
+// else
+// {
+//     Console.WriteLine("The answer is not greater than 10");
+//     Console.WriteLine("Or the first number is not equal to the second");
+// }
