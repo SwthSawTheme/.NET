69131b81182ec9ca8d4e4c9150c482aac77317bf
SwthSawTheme <sawtheme1@gmail.com>
2024-10-27 20:39:06 -0300

    criando metodo em dotnet com testes de operações matematicas

---
+++ numeros/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// void meuprimeiroMetodo()
+// {
+//     int a = 18;
+//     int b = 2;
+
+//     // Adição
+//     int c = a + b;
+//     Console.WriteLine(c);
+
+//     //subtração
+//     c = a - b;
+//     Console.WriteLine(c);
+
+//     //multiplicação
+//     c = a * b;
+//     Console.WriteLine(c);
+
+//     //divisão
+//     c = a / b;
+//     Console.WriteLine(c);
+// }
+
+// meuprimeiroMetodo();
+
